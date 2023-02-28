ddf9b7154ad928bd083796416ae42dd32c332063
Luishaedo <luismariahaedolanda@gmail.com>
2023-02-28 19:16:52 -0300

    m3

--- Front/src/App.js
+++ Front/src/App.js
@@ -20,7 +20,7 @@ function App() {
   }
 
   function onSearch(character) {
-    fetch(`https://rickandmortyapi.com/api/character/${character}`)
+    fetch(`https://localhost:3001/rickandmorty/${character}`)
       .then((response) => response.json())
       .then((data) => {
         if (data.name) {
